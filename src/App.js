10f0e23031c959e550d1eaad4e751f94acafa06f
Milton Chico <milton.chico@outlook.com>
2022-12-27 13:27:05 -0300

    App ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
+
   return (
-    <>
+
+    <ChakraProvider>
       <Router>
         <Navbar />
         <HeroSection />
       </Router>
-    </>
+    </ChakraProvider>
+    
   );
 }
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
+import axios from 'axios'
+import { Spinner } from '@chakra-ui/react'
 
 function HeroSection() {
+
+  const [info, setInfo] = useState('')
+  const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+      getData()
+    },[])
+
+    async function getData() {
+      setLoading(true)
+      axios('http://localhost:8080/results')
+      .then(response => {
+          setInfo(response.data[0])
+          setLoading(false)
+        })
+        .catch(error => {
+          console.error('Error fetching')
+        })
+        .finally(() => {
+        })
+      }
+
   return (
+
     <div className='hero-container'>
       <img
                             src={process.env.PUBLIC_URL + "/images/home-background.jpeg"}
                             alt="example"
                         />
-      <h1>Let's work together</h1>
-      <p>What are you waiting for?</p>
+      <div className='info-container'>
+        <div>
+            <h1>THREE CAPITAL GROUP</h1>
+        </div>
+
+        <div className='stats-container'>
+          <div className='stats-box'>
+            <h2>Total Supply</h2>
+             {isLoading ? <Spinner mt={25} size='lg' color='blue.500' /> : <h1>{info.totalSupply}</h1>}
+          </div>
+          <div className='stats-box'>  
+            <h2>Total Holders</h2>
+            {isLoading ? <Spinner mt={25} size='lg' color='blue.500' /> : <h1>{info.totalHolders}</h1>}
+          </div>
+        </div>
+      </div> 
+
       <div className='hero-btns'>
-        <Button
+        {/* <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
           Connect Wallet
-        </Button>
-        <form action="../../post" method="post" 
+        </Button> */}
+        {/* <form action="../../post" method="post" 
               className="form">
           <button type="submit">Connected?</button>
-        </form>
+        </form> */}
       </div>
     </div>
   );
